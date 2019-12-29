3a5fc79730e22f34779f85645115a5f474edd4a1
Eric Laursen <ericjlaursen@gmail.com>
2019-12-29 09:54:36 -0500

    updating object

--- mutability.py
+++ mutability.py
@@ -1,9 +1,21 @@
 #arrays hold their original id after mutation
 arr = []
+print(arr)
 print(id(arr))
 
 arr.append(14)
+print(arr)
 print(id(arr))
+
+#arrays hold their original id after mutation
+obj = {'a': 'water'}
+print(obj)
+print(id(obj))
+
+obj['b'] = 'melon'
+print(obj)
+print(id(obj))
+
 
 #integer variables do NOT hold their 
 # original id after mutation
