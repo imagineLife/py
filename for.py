911195bc1b836afb5e864c3602f84ec651eaa1a9
Eric Laursen <ericjlaursen@gmail.com>
2019-12-30 15:29:59 -0500

    more looping

--- for.py
+++ for.py
@@ -1,3 +1,12 @@
 pets = ['cat','dog','giraffe']
 for itm in pets:
 	print('I have a {0}'.format(itm))
+
+for i in range(5):
+	print(i)
+
+for itm in range(10,15):
+	print(itm)
+
+for itm in range(10,20,2):
+	print(itm)
