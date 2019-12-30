101196011c5831e6400259f87046b94e62e292de
Eric Laursen <ericjlaursen@gmail.com>
2019-12-30 15:21:57 -0500

    elif blocks

--- if.py
+++ if.py
@@ -1,4 +1,8 @@
 xVal = int(input("enter an integer: "))
 if xVal < 0:
-	x = 0
+	xVal = 0
 	print('negative number changed to Zero here!!')
+elif xVal == 0:
+	print('zero val')
+elif xVal > 0:
+	print('Above zero val')
