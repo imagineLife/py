d720cf2e228c064f8d95155c54fea14928198b6d
Eric Laursen <ericjlaursen@gmail.com>
2019-12-30 15:05:56 -0500

    some string methods

---
+++ strMethods.py
@@ -0,0 +1,12 @@
+str = 'Here is my string.'
+
+print('------')
+print('---Ends With "string."---')
+print('------')
+print(str.endswith("string."))
+
+print('------')
+print('---Ends With substring---')
+print('------')
+subString = "string."
+print(str.endswith(subString))
