f34ad15190eda394e56b5b01dfa499237e56c739
Eric Laursen <ericjlaursen@gmail.com>
2020-01-04 18:04:45 -0500

    sqlite db setup

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import sqlAlchemy
 # __name__ references cur file
 app = Flask(__name__ )
+# telling the app where the db is located
+# /// is relative, //// is absolute
+# test.db is storing the data
+app.config['SQLALCEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+#init db with settings from the app
+db = sqlAlchemy(app)
 
 # index route
 @app.route('/')
