05a8a43fde239508ade1fb1370cd12ca9d62800d
Eric Laursen <ericjlaursen@gmail.com>
2020-01-04 18:07:14 -0500

    Todo db model

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import sqlAlchemy
+from datetime import datetime
 # __name__ references cur file
 app = Flask(__name__ )
 # telling the app where the db is located
@@ -9,6 +10,13 @@ app.config['SQLALCEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 #init db with settings from the app
 db = sqlAlchemy(app)
+
+#make a db model
+class Todo(db.model):
+	#colum & column types
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(200), nullable=False)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 # index route
 @app.route('/')
