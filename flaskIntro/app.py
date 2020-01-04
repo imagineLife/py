27723681530dbaeea473f96aca8c426374999061
Eric Laursen <ericjlaursen@gmail.com>
2020-01-04 14:05:15 -0500

    renders template

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # __name__ references cur file
 app = Flask(__name__ )
@@ -6,7 +6,10 @@ app = Flask(__name__ )
 # index route
 @app.route('/')
 def index():
-	return "Hello, world!"
+	#return "Hello, world!"
+
+	#Here, flask "knows" to look in the 'templates' dir
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
