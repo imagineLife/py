5e230da87aaf528d19de1d484d5745d920856c68
Eric Laursen <ericjlaursen@gmail.com>
2020-01-04 13:21:25 -0500

    hello world app up & running

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -1,4 +1,12 @@
-from flask import flask
+from flask import Flask
 
 # __name__ references cur file
 app = Flask(__name__ )
+
+# index route
+@app.route('/')
+def index():
+	return "Hello, world!"
+
+if __name__ == "__main__":
+	app.run(debug=True)
