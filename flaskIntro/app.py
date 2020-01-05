3596c9c957383d5ed51267770db0b2d7de5423bd
Eric Laursen <ericjlaursen@gmail.com>
2020-01-05 17:03:35 -0500

    delete task route

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -51,5 +51,17 @@ def index():
 		#"tasks" is param-name passed to the index.html template
 		return render_template('index.html', tasks=tasks)
 
+
+# DELETE To-Do item
+@app.route('/delete/<int:id>')
+def delete(id):
+	task_to_delete = Todo.query.get_or_404(id)
+
+	try:
+		db.session.delete(task_to_delete)
+		db.session.commit()
+		return redirect('/')
+	except: return 'There was a problem deleting that task'
+
 if __name__ == "__main__":
 	app.run(debug=True)
