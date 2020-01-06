5b748a18bd8b76fbebbe07d335cf2c832ce9531c
Eric Laursen <ericjlaursen@gmail.com>
2020-01-05 19:39:27 -0500

    updates db task

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -70,7 +70,12 @@ def update(id):
 	task = Todo.query.get_or_404(id)
 
 	if request.method == 'POST':
-		pass
+		task.content = request.form['content']
+		try:
+			db.session.commit()
+			return redirect('/')
+		except:
+			return 'there was an issue updating the task'
 	else:
 		return render_template('update.html', task=task)
 
