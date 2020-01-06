718e6a63345deba778d7abbef00e700541df661b
Eric Laursen <ericjlaursen@gmail.com>
2020-01-05 19:31:07 -0500

    update route

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -63,5 +63,9 @@ def delete(id):
 		return redirect('/')
 	except: return 'There was a problem deleting that task'
 
+@app.route('/update/<int:id>', method=['GET','POST'])
+def update(id):
+	return ''
+
 if __name__ == "__main__":
 	app.run(debug=True)
